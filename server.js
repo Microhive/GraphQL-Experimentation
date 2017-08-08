a54d694f25e3bdb9f58219246e558de417c074b7
ga62waj <eiler.poulsen@tum.de>
2017-08-08 18:28:59 +0200

    added iisnode.yml for azure logging

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// var http = require('http');
 const express = require('express');
 const expressGraphQl = require('express-graphql')
 const schema = require('./schema.js')
@@ -15,13 +14,3 @@ var port = process.env.PORT || 4001;
 app.listen(port, () => {
     console.log('Server is running on port ' + port + '..');
 });
-
-// var server = http.createServer(function(request, response) {
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello World!");
-// });
-
-// var port = process.env.PORT || 1337;
-// server.listen(port);
-
-// console.log("Server running at http://localhost:%d", port);
