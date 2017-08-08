afaf33bca504892d8f231906a01a6ae9db26b90c
ga62waj <eiler.poulsen@tum.de>
2017-08-08 19:05:30 +0200

    Maybe fixed render issue?

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const expressGraphQl = require('express-graphql')
+const graphqlHTTP = require('express-graphql')
 const schema = require('./schema.js')
 
 const app = express();
 
-app.use('/graphql', expressGraphQl({
+app.use('/graphql', graphqlHTTP({
     schema:schema,
+    rootValue: global,
     graphiql:true
 }))
 
